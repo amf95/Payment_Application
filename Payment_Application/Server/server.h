abb240d2c10fc3118a6f8378568e79fbe498ce75
amf95 <31721588+amf95@users.noreply.github.com>
2022-10-19 00:13:48 +0200

    Update server.h
    never been compiled

--- Payment_Application/Server/server.h
+++ Payment_Application/Server/server.h
@@ -27,23 +27,26 @@ task: Implement the server module.
 #define PAN_MAX_SIZE 19
 #define LOG_FILE_PATH "./logs/logs.txt"
 
-typedef struct ST_transaction_t
-{
-    ST_cardData_t cardHolderData;
-    ST_trminalData_t terminalData;
-    EN_transState_t transState;
-    uint32_t transactionSequenceNumber;
-}ST_transaction;
+
 
 typedef enum EN_transState_t
 {
     APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, INTERNAL_SERVER_ERROR
-}EN_transStat_t;
+}EN_transState_t;
 
 typedef enum EN_serverError_t
 {
     OK, SAVING_FAILED, TRANSACTION_NOT_FOUND, ACCOUNT_NOT_FOUND, LOW_BALANCE
 }EN_serverError_t ;
+
+
+typedef struct ST_transaction_t
+{
+    ST_cardData_t cardHolderData;
+    ST_terminalData_t terminalData;
+    EN_transState_t transState;
+    uint32_t transactionSequenceNumber;
+}ST_transaction_t;
 
 typedef struct ST_accountsDB_t
 {
@@ -54,7 +57,7 @@ typedef struct ST_accountsDB_t
 
 EN_transState_t recieveTransactionData(ST_transaction_t *transData);
 EN_serverError_t isValidAccount(ST_cardData_t *cardData);
-EN_serverError_t isAmountAvailable(ST_trminalData_t *termData);
+EN_serverError_t isAmountAvailable(ST_terminalData_t *termData);
 EN_serverError_t saveTransaction(ST_transaction_t *transData);
 EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t *transData);
 
