f6b46145b89dd86a3c50aa143f3443c201471472
amf95 <31721588+amf95@users.noreply.github.com>
2022-10-19 00:16:14 +0200

    Update terminal.h
    never been compiled

--- Payment_Application/Terminal/terminal.h
+++ Payment_Application/Terminal/terminal.h
@@ -23,17 +23,19 @@ task: Implement the terminal module.
 #define BUFFER_SIZE 32
 #define TRANSACTION_DATE_SIZE 10
 
+
+
+typedef enum EN_terminalError_t
+{
+OK, WRONG_DATE, EXPIRED_CARD, INVALID_CARD, INVALID_AMOUNT, EXCEED_MAX_AMOUNT, INVALID_MAX_AMOUNT
+}EN_terminalError_t ;
+
 typedef struct ST_terminalData_t
 {
 float transAmount;
 float maxTransAmount;
 uint8_t transactionDate[TRANSACTION_DATE_SIZE + 1];
 }ST_terminalData_t;
-
-typedef enum EN_terminalError_t
-{
-OK, WRONG_DATE, EXPIRED_CARD, INVALID_CARD, INVALID_AMOUNT, EXCEED_MAX_AMOUNT, INVALID_MAX_AMOUNT
-}EN_terminalError_t ;
 
 EN_terminalError_t getTransactionDate(ST_terminalData_t *termData);
 EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termData);
