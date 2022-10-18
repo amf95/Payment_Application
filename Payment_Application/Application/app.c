220d2589a32c45c086adcf68574f1df82d8657b1
amf95 <31721588+amf95@users.noreply.github.com>
2022-10-19 00:10:08 +0200

    Update app.c
    never been compiled

--- Payment_Application/Application/app.c
+++ Payment_Application/Application/app.c
@@ -17,45 +17,76 @@ task: Testing the application
 
 */
 
-#include "application.h"
-#include "../modules/card.h"
-#include "../modules/terminal.h"
-#include "../modules/server.h"
-#include "../modules/config.h"
+#include "app.h"
+#include "../Card/card.h"
+#include "../Terminal/terminal.h"
+#include "../Server/server.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-client_t newCard;
+ST_cardData_t card;
+ST_terminalData_t terminal;
+ST_transaction_t transaction;
 
-client_t testCards[6] = {
-    {"Ahmed Fawzy", "1111111111111111", 7, 2025, 5000},  // case: 1
-    {"Mohamed Ali", "2222222222222222", 8, 2026, 10000}, // case: 2
-    {"Amr Zein",    "3333333333333333", 5, 2027, 5000},  // case: 3
-    {"Sara Ahmed",  "4444444444444444", 6, 2021, 1000},  // case: 4
-    {"Saly Mahmoud","5555555555555555", 6, 2028, 1000}  // case: 5
+ST_transaction_t transactionTestCases[6] = {
+    {.cardHolderData = 
+    {.cardHolderName =  "Ahmed Mohamed Mahmoud",
+    .primaryAccountNumber = "1111111111111111", 
+    .cardExpirationDate = "07/25"},
+    .terminalData = {.transAmount = 5000} },  // case: 1
+
+    {.cardHolderData = 
+    {.cardHolderName =  "Mohamed Ali Samy Ibrahim",
+    .primaryAccountNumber = "2222222222222222", 
+    .cardExpirationDate = "08/26"},
+    .terminalData = {.transAmount = 10000} }, // case: 2
+
+    {.cardHolderData = 
+    {.cardHolderName =  "Amr Zein Mohamed Ali",
+    .primaryAccountNumber = "3333333333333333", 
+    .cardExpirationDate = "03/27"},
+    .terminalData = {.transAmount = 10000} 
+    }, // case: 3
+
+    {.cardHolderData = 
+    {.cardHolderName =  "Sara Ahmed Mohamed Samir",
+    .primaryAccountNumber = "4444444444444444", 
+    .cardExpirationDate = "04/21"},
+    .terminalData = {.transAmount = 5000} 
+    }, // case: 4
+
+    {.cardHolderData = 
+    {.cardHolderName =  "Saly Mahmoud Zein Sayed",
+    .primaryAccountNumber = "5555555555555555", 
+    .cardExpirationDate = "05/28"},
+    .terminalData = {.transAmount = 1000} 
+    }, // case: 3
     };
     
+void loadTestValues(ST_cardData_t *cardData, ST_terminalData_t *termData, 
+ST_transaction_t source,char *testCase);
 
 void appStart(){
 
-    setMaxAmount(8000); // max withdraw amount.
+    terminal.maxTransAmount = 8000;
+    setMaxAmount(&terminal); // max withdraw amount.
 
     int mode = 0;
     
     printf(
     "0-Manual Inputs.\n"
-    "{\"<NAME>\", \"xxxxxxxxxxxxxxxx\", xx, xx, xxxx}\n\n"
+    "[\"<NAME:[20-24]>\", \"<PAN:[16-19]>\", <DATE:MM/YY>, <MAX_AMOUNT:8000>]\n\n"
     "1-Transaction approved user story:\n"
-    "{\"Ahmed Fawzy\", \"1111111111111111\", 7, 2025, 5000}\n\n"
+    "[\"Ahmed Mohamed Mahmoud\", \"1111111111111111\", 07/25, 5000]\n\n"
     "2-Exceed the maximum amount user story:\n"
-    "{\"Mohamed Ali\", \"2222222222222222\", 8, 2026, 10000}\n\n"
+    "[\"Mohamed Ali Samy Ibrahim\", \"2222222222222222\", 08/26, 10000]\n\n"
     "3-Insufficient fund user story:\n"
-    "{\"Amr Zein\",    \"3333333333333333\", 5, 2027, 5000}\n\n"
+    "[\"Amr Zein Mohamed Ali\", \"3333333333333333\", 03/27, 5000]\n\n"
     "4-Expired card user story:\n"
-    "{\"Sara Ahmed\",  \"4444444444444444\", 6, 2021, 1000}\n\n"
+    "[\"Sara Ahmed Mohamed Samir\",  \"4444444444444444\", 04/21, 1000]\n\n"
     "5-Invalid card user story:\n"
-    "{\"Saly Mahmoud\",\"5555555555555555\", 6, 2028, 1000}\n\n"
+    "[\"Saly Mahmoud\",\"5555555555555555\", 05/28, 1000]\n\n"
     "Please Enter Test Case Number: ");
 
     scanf("%d", &mode);
@@ -68,32 +99,45 @@ void appStart(){
     {
         case 0:
             printf("Please Enter Your Name: ");
-            getCardHolderName(&newCard);
+            if(getCardHolderName(&card) == WRONG_NAME){
+                printf("WRONG_NAME!\n");
+                exit(0);
+            }
             printf("Please Enter Expiry Date: ");
-            getCardExpiryDate(&newCard);
+            if(getCardExpiryDate(&card) == WRONG_EXP_DATE){
+                printf("WRONG_EXP_DATE!\n");
+                exit(0);
+            }
             printf("Please Enter Card PAN: ");
-            getCardPAN(&newCard);
+            if(getCardPAN(&card) == WRONG_PAN){
+                printf("WRONG_PAN!\n");
+                exit(0);
+            }
             break;
 
         case 1:
-            loadTestValues(&newCard, &testCards[mode-1], "Transaction approved user story");
-
+            loadTestValues(&card, &terminal,  transactionTestCases[mode-1],
+            "Transaction approved user story");
             break;
 
         case 2:
-            loadTestValues(&newCard, &testCards[mode-1], "Exceed the maximum amount user story");
+            loadTestValues(&card, &terminal,  transactionTestCases[mode-1],
+            "Exceed the maximum amount user story");
             break;
 
         case 3:
-            loadTestValues(&newCard, &testCards[mode-1], "Insufficient fund user story");    
+            loadTestValues(&card, &terminal,  transactionTestCases[mode-1],
+            "Insufficient fund user story");    
             break;
 
         case 4:
-            loadTestValues(&newCard, &testCards[mode-1], "Expired card user story");
+            loadTestValues(&card, &terminal,  transactionTestCases[mode-1],
+            "Expired card user story");
             break; 
 
         case 5:
-            loadTestValues(&newCard, &testCards[mode-1], "Invalid card user story");
+            loadTestValues(&card, &terminal,  transactionTestCases[mode-1],
+            "Invalid card user story");
             break;       
 
         default:
@@ -104,63 +148,68 @@ void appStart(){
     //------------------------------ End Card Side ------------------------------
     
     //--------------------------- Start Terminal Side ---------------------------
-    if(isCardExpried(newCard)){
-        printCardInfo(newCard);
-        printf("Expired Card!\n");
+    if(isCardExpried(&card, &terminal) == EXPIRED_CARD){
+        //printCardInfo(newCard);
+        printf("EXPIRED_CARD!\n");
         exit(0);
     }
  
     if(mode == 0){
         printf("Please Enter Wanted Amount: ");
-        getTransactionAmount(&newCard);
+        if(getTransactionAmount(&terminal) == INVALID_AMOUNT){
+            printf("INVALID_AMOUNT\n");
+            exit(0); 
+        }
     }
     
-    printf("\n");
-    printCardInfo(newCard);
-    printf("\n");
+    //printf("\n");
+    //printCardInfo(newCard);
+    //printf("\n");
     
-    if(isBelowMaxAmount(newCard)){
-    //----------------------- End Terminal Side -------------------------
-        
-        //----------------------  Start Server Side -------------------------
-        recieveTransactionData(newCard);
-
-        if(isValidAccount()){
-            if(isAmountAvailable()){
-                printf("Account Info Before Saving:\n");
-                printClientInfo();
-                
-                saveTransaction();
-
-                printf("Account Info After Saving:\n");
-                printClientInfo();
-            }
-            else{
-                printf("Insuffecient Funds!\n");
-                exit(0);
-            }
-        }
-        else{
-            printf("Invalid Account!\n");
-            exit(0);
-        }
-
-        //--------------------------- Server Side ---------------------------
-    }
-    else{
-        printf("Exceeded Maximum Withdraw Amount!\n");
+    if(!(isBelowMaxAmount(&terminal) == OK)){
+        printf("EXCEED_MAX_AMOUNT!\n");
         exit(0);
     }
+    //----------------------- End Terminal Side -------------------------
+        
+    //----------------------  Start Server Side -------------------------
+    transaction.cardHolderData = card;
+    transaction.terminalData = terminal;
+    int transactionState = recieveTransactionData(&transaction);
+
+    switch (transactionState)
+    {
+    case APPROVED:
+        printf("Transaction State: APPROVED!.\n");
+        break;
+    
+    case DECLINED_INSUFFECIENT_FUND:
+        printf("Transaction State: DECLINED_INSUFFECIENT_FUND!.\n");
+        break;
+
+    case DECLINED_STOLEN_CARD:
+        printf("Transaction State: DECLINED_STOLEN_CARD!\n");
+        break;
+
+    case INTERNAL_SERVER_ERROR:
+        printf("Transaction State: INTERNAL_SERVER_ERROR!\n");
+        break;
+
+    default:
+        break;
+    }
+    //--------------------------- Server Side ---------------------------
+    
+   
 
 }
 
-//---------------------------------------------------------------------
 
-void loadTestValues(client_t *taget, client_t *source, char *testCase){
+//------------------------------ Helper Functions ----------------------------------
+
+void loadTestValues(ST_cardData_t *cardData, ST_terminalData_t *termData, 
+ST_transaction_t source,char *testCase){
     printf("Loading \"%s\".\n", testCase);
-    strcpy(taget->holderName, source->holderName);
-    strcpy(taget->PAN, source->PAN);
-    taget->expiryMonth = source->expiryMonth;
-    taget->expiryYear = source->expiryYear;
-    taget->balance = source->balance;
+    *cardData = source.cardHolderData;
+    *termData = source.terminalData;
 }
