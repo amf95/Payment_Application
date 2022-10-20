5886f7631bc3299b2c17393578383f8ad9c50735
amf95 <31721588+amf95@users.noreply.github.com>
2022-10-20 20:05:00 +0200

    Add files via upload
    added test module without implementing it.
    added and implemented listSavedTransactions function

---
+++ Payment_Application/Payment_Application/Server/server.h
@@ -0,0 +1,76 @@
+/*
+
+Auther: Ahmed Mahmoud Fawzy.
+Creation Date: 15-10-2022 Sat 10:30 AM.
+Last Modification Date: 17-10-2022 Mon 17:00 PM  => Ahmed Fawzy.
+
+task: Implement the server module.
+1-Fill in server.h file with functions' prototypes and typedefs.
+2-Implement server-side accounts' database.
+3-Implement server-side transactions' database.
+4-Implement recieveTransactionData function.
+5-Implement isValidAccount function.
+6-Implement isAmountAvailable function.
+7-Implement saveTransaction function.
+
+*/
+
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "../Card/card.h"
+#include "../Terminal/terminal.h"
+#include <stdint.h>
+
+#define ACCOUNTS_DB_SIZE 255
+#define TRANSACTIONS_DB_SIZE 255
+#define PAN_MAX_SIZE 19
+#define LOG_FILE_PATH "./Transactions_DB/Transactions_DB.txt"
+#define LOG_SIZE 1024
+#define LINE_MAX_SIZE 1024
+
+typedef enum EN_transState_t
+{
+    APPROVED, 
+    DECLINED_INSUFFECIENT_FUND, 
+    DECLINED_STOLEN_CARD, 
+    INTERNAL_SERVER_ERROR
+}EN_transState_t;
+
+typedef enum EN_serverError_t
+{
+    SERVER_OK,
+    SAVING_FAILED, 
+    TRANSACTION_NOT_FOUND, ACCOUNT_NOT_FOUND,
+    LOW_BALANCE, 
+    BLOCKED_ACCOUNT
+}EN_serverError_t ;
+
+typedef enum EN_accountState_t
+{
+     RUNNING,
+     BLOCKED
+}EN_accountState_t;
+
+typedef struct ST_transaction_t
+{
+    ST_cardData_t cardHolderData;
+    ST_terminalData_t terminalData;
+    EN_transState_t transState;
+    uint32_t transactionSequenceNumber;
+}ST_transaction_t;
+
+typedef struct ST_accountsDB_t
+{
+    float balance;
+    uint8_t primaryAccountNumber[PAN_MAX_SIZE + 1];
+}ST_accountsDB_t;
+
+
+EN_transState_t recieveTransactionData(ST_transaction_t *transData);
+EN_serverError_t isValidAccount(ST_cardData_t *cardData);
+EN_serverError_t isAmountAvailable(ST_terminalData_t *termData);
+EN_serverError_t saveTransaction(ST_transaction_t *transData);
+void listSavedTransactions(void);
+
+#endif
